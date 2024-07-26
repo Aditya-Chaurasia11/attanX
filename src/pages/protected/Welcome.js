51d418a3b9acd6611a49aab4c7e7c563350c14df
Aditya Chaurasia <1212aditya11@gmail.com>
2024-07-26 14:56:38 +0530

    ui updated

--- src/pages/protected/Welcome.js
+++ src/pages/protected/Welcome.js
@@ -1,27 +1,30 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPageTitle } from '../../features/common/headerSlice'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import TemplatePointers from '../../features/user/components/TemplatePointers'
 
-function InternalPage(){
+function InternalPage() {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setPageTitle({ title: "" }))
+  }, [dispatch])
 
-    useEffect(() => {
-        dispatch(setPageTitle({ title : ""}))
-      }, [])
-
-    return(
-      <div className="hero h-4/5 bg-base-200">
+  return (
+    <div className="hero h-4/5 bg-base-200">
       <div className="hero-content">
-        <div className="max-w-md">
-            <TemplatePointers />
-            <Link to="/app/leads"><button className="btn bg-base-100 btn-outline">Get Started</button></Link>
+        <div className="max-w-md text-center mt-5"> {/* Centering and margin-top */}
+          <TemplatePointers />
+          <div style={{ marginTop: '20px' }}>
+            <Link to="/app/leads">
+              <button className="btn bg-base-100 btn-outline">Get Started</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    )
+  )
 }
 
 export default InternalPage
